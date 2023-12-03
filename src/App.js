7377b004896f24a8c4f9e55afe49238d49f188a7
quanquan.luo <quanquan.luo@okg.com>
2023-12-03 23:17:35 +0800

    自动重连

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Space, Tabs } from 'antd';
+/* eslint-disable no-param-reassign */
+import { useEffect } from 'react';
+import { Space, Tabs, message } from 'antd';
 
 import Evm from './chains/Evm';
 import Tron from './chains/Tron';
@@ -24,6 +26,25 @@ const tabs = [
 ];
 
 export default function App() {
+  useEffect(() => {
+    const reload = ((timer = null) => () => {
+      if (timer) {
+        return;
+      }
+
+      timer = setTimeout(() => {
+        timer = null;
+        clearTimeout(timer);
+        message.error('检测到钱包连接断开, 正在自动刷新 ~', 1, () => {
+          window.location.reload();
+        });
+      }, 1e3);
+    })();
+
+    okxwallet.on('disconnect', reload);
+    return () => okxwallet.off('disconnect', reload);
+  }, []);
+
   return (
     <div className="wrap">
       <Space direction="vertical">
