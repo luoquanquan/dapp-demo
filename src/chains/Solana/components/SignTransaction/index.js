0b9581c7e1cbaadf3a12e5c9337c06bd66de8029
quanquan.luo <quanquan.luo@okg.com>
2024-01-23 00:06:51 +0800

    add versioned tx for solana

--- src/chains/Solana/components/SignTransaction/index.js
+++ src/chains/Solana/components/SignTransaction/index.js
@@ -8,11 +8,16 @@ import {
   SystemProgram,
   LAMPORTS_PER_SOL,
   PublicKey,
+  TransactionMessage,
+  VersionedTransaction,
 } from '@solana/web3.js';
 import { mySolAddress } from '../../../../utils/const';
 
 const lamports = LAMPORTS_PER_SOL / 10 ** 4;
-const withConnection = (connection, toPubkey = new PublicKey(mySolAddress)) => async () => {
+const withConnectionGenerateTx = (
+  connection,
+  toPubkey = new PublicKey(mySolAddress),
+) => async () => {
   const tx = new Transaction();
 
   tx.add(
@@ -29,8 +34,29 @@ const withConnection = (connection, toPubkey = new PublicKey(mySolAddress)) => a
   return tx;
 };
 
+const withConnectionGenerateVersionedTx = (
+  connection,
+  toPubkey = new PublicKey(mySolAddress),
+) => async () => {
+  const recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+  const versionedTransactionMessage = new TransactionMessage({
+    payerKey: solana.publicKey,
+    recentBlockhash,
+    instructions: [
+      SystemProgram.transfer({
+        fromPubkey: solana.publicKey,
+        toPubkey,
+        lamports,
+      }),
+    ],
+  }).compileToV0Message();
+
+  return new VersionedTransaction(versionedTransactionMessage);
+};
+
 export default function SignTransaction({ account, connection }) {
-  const generateTx = withConnection(connection);
+  const generateTx = withConnectionGenerateTx(connection);
+  const generateVersionedTx = withConnectionGenerateVersionedTx(connection);
 
   const [signTransactionLoading, setSignTransactionLoading] = useState(false);
   const signTransaction = async () => {
@@ -90,11 +116,75 @@ export default function SignTransaction({ account, connection }) {
     }
   };
 
+  const [signVersionedTransactionLoading, setSignVersionedTransactionLoading] = useState(false);
+  const signVersionedTransaction = async () => {
+    try {
+      setSignVersionedTransactionLoading(true);
+      const tx = await generateVersionedTx();
+      const signedTx = await solana.signTransaction(tx);
+      console.log('Current log: signedTx: ', signedTx);
+      const txHash = await connection.sendRawTransaction(signedTx.serialize());
+      console.log('Current log: txHash: ', txHash);
+    } catch (error) {
+      message.error('操作失败, 请打开控制台查看错误信息');
+      console.log('error: ', error);
+    } finally {
+      setSignVersionedTransactionLoading(false);
+    }
+  };
+
+  const [
+    signAllVersionedTransactionsLoading,
+    setSignAllVersionedTransactionsLoading,
+  ] = useState(false);
+  const signAllVersionedTransactions = async () => {
+    try {
+      setSignAllVersionedTransactionsLoading(true);
+      const txs = [
+        await generateVersionedTx(),
+        await generateVersionedTx(),
+      ];
+      const signedTxs = await solana.signAllTransactions(txs);
+      console.log('Current log: signedTxs: ', signedTxs);
+
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < signedTxs.length; i++) {
+        const tx = signedTxs[i];
+        // eslint-disable-next-line no-await-in-loop
+        const txHash = await connection.sendRawTransaction(tx.serialize());
+        console.log('Current log: txHash: ', txHash);
+      }
+    } catch (error) {
+      message.error('操作失败, 请打开控制台查看错误信息');
+      console.log('error: ', error);
+    } finally {
+      setSignAllVersionedTransactionsLoading(false);
+    }
+  };
+
+  const [
+    signAndSendVersionedTransactionLoading,
+    setSignAndSendVersionedTransactionLoading,
+  ] = useState(false);
+  const signAndSendVersionedTransaction = async () => {
+    try {
+      setSignAndSendVersionedTransactionLoading(true);
+      const tx = await generateVersionedTx();
+      const signedTx = await solana.signAndSendTransaction(tx);
+      console.log('Current log: signedTx: ', signedTx);
+    } catch (error) {
+      message.error('操作失败, 请打开控制台查看错误信息');
+      console.log('error: ', error);
+    } finally {
+      setSignAndSendVersionedTransactionLoading(false);
+    }
+  };
+
   return (
     <Card title="合约交互 - 请打开控制台查看签名信息">
       <Row gutter={16}>
         <Col span={12}>
-          <Card title="仅需钱包签名, dapp 广播">
+          <Card title="Transaction">
             <Space direction="vertical" style={{ width: '100%' }}>
               <Button
                 block
@@ -113,12 +203,7 @@ export default function SignTransaction({ account, connection }) {
               >
                 signAllTransactions
               </Button>
-            </Space>
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card title="钱包签名且广播">
-            <Space direction="vertical" style={{ width: '100%' }}>
+
               <Button
                 block
                 disabled={!account}
@@ -126,6 +211,38 @@ export default function SignTransaction({ account, connection }) {
                 onClick={signAndSendTransaction}
               >
                 signAndSendTransaction
+              </Button>
+            </Space>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Versioned Transaction">
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <Button
+                block
+                disabled={!account}
+                loading={signVersionedTransactionLoading}
+                onClick={signVersionedTransaction}
+              >
+                signVersionedTransaction
+              </Button>
+
+              <Button
+                block
+                disabled={!account}
+                loading={signAllVersionedTransactionsLoading}
+                onClick={signAllVersionedTransactions}
+              >
+                signAllVersionedTransactions
+              </Button>
+
+              <Button
+                block
+                disabled={!account}
+                loading={signAndSendVersionedTransactionLoading}
+                onClick={signAndSendVersionedTransaction}
+              >
+                signAndSendVersionedTransaction
               </Button>
             </Space>
           </Card>
