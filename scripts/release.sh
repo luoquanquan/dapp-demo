478c5796f67fa518534b7320e4b394b2ee6ae97d
quanquan.luo <quanquan.luo@okg.com>
2024-08-15 21:14:22 +0800

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-package_name="`date +%Y%m%d%H%M`-dapp-demo"
 version="`date +%Y-%m-%d\ %H:%M:%S`"
-
 
 echo "Start compile $version"
 
@@ -17,30 +15,10 @@ echo "End compile $version"
 
 cp ../public/tonconnect-manifest.json ../docs
 
-# 缓存编译结果
 git stash -u
 
-# 切换到目标分支
 git checkout release
 
-# 移除之前的 docs
-rm -rf docs/
-git add .
-git commit -m'Delete old files'
-
-# 取出新的编译结果
-git stash pop
-
-# 打压缩包, 供下载使用
-# mv docs $package_name
-# mkdir $package_name/dapp-demo
-# mv $package_name/static $package_name/dapp-demo/static
-# zip -r ./$package_name.zip $package_name
-
-# 打完压缩包之后改回原来的名字
-# mv $package_name docs
-# mv $package_name.zip docs
-# mv docs/dapp-demo/static docs/static
 git add .
 git commit -m "Deploy for $version"
 
