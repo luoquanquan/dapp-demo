21d2666a7e467c88841c8ce2dedaff3368c13c1e
quanquan.luo <quanquan.luo@okg.com>
2024-09-20 14:30:23 +0800

    disable VConsole

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import VConsole from 'vconsole';
+// import VConsole from 'vconsole';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 import './index.css';
 
-setTimeout(() => {
-  // eslint-disable-next-line no-new
-  process.env.NODE_ENV !== 'development' && new VConsole();
-}, 1e3);
+// setTimeout(() => {
+//   // eslint-disable-next-line no-new
+//   process.env.NODE_ENV !== 'development' && new VConsole();
+// }, 1e3);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
