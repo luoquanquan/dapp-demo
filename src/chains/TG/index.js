20259418874d01afb3833bbb7647bd482702a822
郑昌浩 <490641570@qq.com>
2024-09-24 18:32:41 +0800

    add ton address

--- src/chains/TG/index.js
+++ src/chains/TG/index.js
@@ -1,10 +1,23 @@
-import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectUIProvider, TonConnectButton, useTonAddress } from '@tonconnect/ui-react';
 
 function TG() {
+  const userFriendlyAddress = useTonAddress();
+  const rawAddress = useTonAddress(false);
+
   return (
     <TonConnectUIProvider manifestUrl="https://app.ston.fi/tonconnect-manifest.json">
       <span>My App with React UI</span>
       <TonConnectButton />
+      <div>
+        <span>
+          User-friendly address:
+          {userFriendlyAddress}
+        </span>
+        <span>
+          Raw address:
+          {rawAddress}
+        </span>
+      </div>
     </TonConnectUIProvider>
   );
 }
