2e744d15fd98a96634a3441d43967d1e5f71f6f1
quanquan.luo <quanquan.luo@okg.com>
2024-10-09 17:35:26 +0800

    update

--- src/chains/TG/index.js
+++ src/chains/TG/index.js
@@ -79,8 +79,8 @@ function TG() {
           imageUrl: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
           about_url: 'https://www.okx.com/web3',
           aboutUrl: 'https://www.okx.com/web3',
-          universal_url: 'https://t.me/TCTestqdqwdqwdqBot?attach=TCTestqdqwdqwdqBot',
-          universalLink: 'https://t.me/TCTestqdqwdqwdqBot?attach=TCTestqdqwdqwdqBot',
+          universal_url: 'https://www.okx.com/web3/mini-wallet',
+          universalLink: 'https://www.okx.com/web3/mini-wallet',
           bridgeUrl: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
           bridge: [
             {
