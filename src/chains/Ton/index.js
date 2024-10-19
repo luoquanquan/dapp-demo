4cbca070e3ae6e575df2870498068778be19f4aa
quanquan.luo <quanquan.luo@okg.com>
2024-10-19 17:37:57 +0800

    迁移 tg 连接方法

--- src/chains/Ton/index.js
+++ src/chains/Ton/index.js
@@ -9,7 +9,70 @@ import { blackTonAddress, strongBlackTonAddress } from './const';
 
 function Ton() {
   return (
-    <TonConnectUIProvider manifestUrl="https://luoquanquan.github.io/dapp-demo/tonconnect-manifest.json">
+    <TonConnectUIProvider
+      manifestUrl="https://luoquanquan.github.io/dapp-demo/tonconnect-manifest.json"
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            app_name: 'PROD OKX Mini Wallet',
+            appName: 'PROD OKX Mini Wallet',
+            name: 'PROD OKX Mini Wallet',
+            image: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            imageUrl: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            about_url: 'https://www.okx.com/web3',
+            aboutUrl: 'https://www.okx.com/web3',
+            universal_url: 'https://t.me/OKX_WALLET_BOT?attach=wallet',
+            universalLink: 'https://t.me/OKX_WALLET_BOT?attach=wallet',
+            bridgeUrl: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+            bridge: [
+              {
+                type: 'sse',
+                url: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+              },
+            ],
+            platforms: ['ios', 'android', 'chrome', 'firefox', 'macos'],
+          },
+          {
+            app_name: 'okxTestWallet',
+            appName: 'okxTestWallet',
+            name: 'OKX Test Wallet',
+            image: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            imageUrl: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            about_url: 'https://www.okx.com/web3',
+            aboutUrl: 'https://www.okx.com/web3',
+            universal_url: 'https://t.me/TCTestqdqwdqwdqBot?attach=TCTestqdqwdqwdqBot',
+            universalLink: 'https://t.me/TCTestqdqwdqwdqBot?attach=TCTestqdqwdqwdqBot',
+            bridgeUrl: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+            bridge: [
+              {
+                type: 'sse',
+                url: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+              },
+            ],
+            platforms: ['ios', 'android', 'chrome', 'firefox', 'macos'],
+          },
+          {
+            app_name: 'betaOkxTestWallet',
+            appName: 'betaOkxTestWallet',
+            name: 'Beta OKX Test Wallet',
+            image: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            imageUrl: 'https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png',
+            about_url: 'https://www.okx.com/web3',
+            aboutUrl: 'https://www.okx.com/web3',
+            universal_url: 'https://t.me/okx_tg_miniapp_test_bot?attach=okx_tg_miniapp_test_bot',
+            universalLink: 'https://t.me/okx_tg_miniapp_test_bot?attach=okx_tg_miniapp_test_bot',
+            bridgeUrl: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+            bridge: [
+              {
+                type: 'sse',
+                url: 'https://www.okx.com/tonbridge/discover/rpc/bridge',
+              },
+            ],
+            platforms: ['ios', 'android', 'chrome', 'firefox', 'macos'],
+          },
+        ],
+      }}
+    >
       <Space direction="vertical" style={{ width: '100%' }}>
         <TonConnectButton />
         <SignTransaction />
