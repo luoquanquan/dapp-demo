58bbf2735b104148ca65a0bab2bd1416edfcba2a
shaowei.zhou <shaowei.zhou@okg.com>
2024-11-11 17:47:42 +0800

    feat: connect modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import {
-  Space,
-} from 'antd';
-import { SafeArea, Tabs } from 'antd-mobile';
+import { Space, Button } from "antd";
+import { SafeArea, Tabs } from "antd-mobile";
+import { openConnectModal } from "@repo/dapp-connect-sdk";
+import Evm from "./chains/Evm";
+import Tron from "./chains/Tron";
+import Solana from "./chains/Solana";
+import NEAR from "./chains/NEAR";
+import ProjectInfo from "./components/ProjectInfo";
+import Ton from "./chains/Ton";
+import Sui from "./chains/Sui";
 
-import Evm from './chains/Evm';
-import Tron from './chains/Tron';
-import Solana from './chains/Solana';
-import NEAR from './chains/NEAR';
-import ProjectInfo from './components/ProjectInfo';
-import Ton from './chains/Ton';
-import Sui from './chains/Sui';
+const localTabKey = "localTabKey";
 
-const localTabKey = 'localTabKey';
-
-const tabs = [
-  Evm,
-  Tron,
-  Solana,
-  NEAR,
-  Ton,
-  Sui,
-];
+const tabs = [Evm, Tron, Solana, NEAR, Ton, Sui];
 
 const cachedChainKey = localStorage.getItem(localTabKey);
 const isValidDefaultActiveKey = tabs.some(({ key }) => cachedChainKey === key);
@@ -30,19 +21,18 @@ export default function App() {
   return (
     <Space direction="vertical" className="wrap">
       <SafeArea position="top" />
+      <Button onClick={openConnectModal}>Connect Wallet</Button>
       <Tabs
         defaultActiveKey={defaultActiveKey}
         onChange={(target) => {
           localStorage.setItem(localTabKey, target);
         }}
       >
-        {
-            tabs.map((tab) => (
-              <Tabs.Tab title={tab.key} key={tab.key}>
-                {tab.children}
-              </Tabs.Tab>
-            ))
-          }
+        {tabs.map((tab) => (
+          <Tabs.Tab title={tab.key} key={tab.key}>
+            {tab.children}
+          </Tabs.Tab>
+        ))}
       </Tabs>
       <ProjectInfo />
       <SafeArea position="bottom" />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import VConsole from 'vconsole';
 import ReactDOM from 'react-dom/client';
-
+import { connect, openConnectModal } from '@repo/dapp-connect-sdk';
 import App from './App';
 
 import './index.css';
+
+connect();
+connect();
 
 setTimeout(() => {
   // eslint-disable-next-line no-new
