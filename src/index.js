e0202010b9ce14d75deeaec9407184e230fb9ad5
shaowei.zhou <shaowei.zhou@okg.com>
2024-11-12 14:44:21 +0800

    feat: remove useless code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import VConsole from 'vconsole';
 import ReactDOM from 'react-dom/client';
-import { connect, openConnectModal } from '@repo/dapp-connect-sdk';
+import { openConnectModal } from '@repo/dapp-connect-sdk';
 import App from './App';
 
 import './index.css';
-
-connect();
-connect();
 
 setTimeout(() => {
   // eslint-disable-next-line no-new
