45a7a290512765e4b50d09b612d4c5845cd08c8e
Elvis Wong <elvis.wong@okg.com>
2024-11-12 17:25:28 +0800

    init 2024-11-12 17:25:28

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import VConsole from 'vconsole';
-import ReactDOM from 'react-dom/client';
-import { openConnectModal } from '@repo/dapp-connect-sdk';
-import App from './App';
+import React from "react";
+import VConsole from "vconsole";
+import ReactDOM from "react-dom/client";
+import { openConnectModal } from "@repo/dapp-connect-sdk";
+import App from "./App";
 
-import './index.css';
+import "./index.css";
 
 setTimeout(() => {
   // eslint-disable-next-line no-new
-  process.env.NODE_ENV !== 'development' && new VConsole();
+  process.env.NODE_ENV !== "development" && new VConsole();
 }, 1e3);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// add telegram web app script
+const script = document.createElement("script");
+script.src = "https://telegram.org/js/telegram-web-app.js";
+script.id = "tgsdk";
+
+document.getElementsByTagName("head")[0].appendChild(script);
+
 root.render(<App />);
