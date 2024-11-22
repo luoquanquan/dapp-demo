7750628d2b72aed9e4cdd52342d0e712b3e5df52
shaowei.zhou <shaowei.zhou@okg.com>
2024-11-22 15:49:21 +0800

    remove telegram

--- src/index.js
+++ src/index.js
@@ -16,11 +16,4 @@ setTimeout(() => {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// add telegram web app script
-const script = document.createElement('script');
-script.src = 'https://telegram.org/js/telegram-web-app.js';
-script.id = 'tgsdk';
-
-document.getElementsByTagName('head')[0].appendChild(script);
-
 root.render(<App />);
