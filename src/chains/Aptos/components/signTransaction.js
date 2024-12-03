8bb962b81da33b5cf51e44f33308097ff3b4df33
elvis.wong <elvis.wong@okg.com>
2024-12-03 18:48:07 +0800

    Add simulate test

--- src/chains/Aptos/components/signTransaction.js
+++ src/chains/Aptos/components/signTransaction.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import { Button, Space, Card } from 'antd-mobile';
 import { message } from 'antd';
 import {
-  AptosConfig, Aptos, Network, APTOS_COIN,
+  AptosConfig, Aptos, Network, Account,
 } from '@aptos-labs/ts-sdk';
 import { ConnectKitErrorCodes } from '@repo/connect-kit';
 import { toastFail, toastSuccess } from '../../../utils/toast';
@@ -21,18 +21,54 @@ function SignTransaction({ account, disabled }) {
     const txData = await aptos.transaction.build.simple({
       sender,
       data: {
-        function:
-          '0x80273859084bc47f92a6c2d3e9257ebb2349668a1b0fb3db1d759a04c7628855::router::swap_exact_coin_for_coin_x1',
-        typeArguments: [
-          '0x1::aptos_coin::AptosCoin',
-          '0x111ae3e5bc816a5e63c2da97d0aa3886519e0cd5e4b046659fa35796bd11542a::stapt_token::StakedApt',
-          '0x0163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e::curves::Uncorrelated',
-          '0x80273859084bc47f92a6c2d3e9257ebb2349668a1b0fb3db1d759a04c7628855::router::BinStepV0V05',
-        ],
-        functionArguments: ['10000', ['9104'], ['5'], ['true']],
+        function: '0x1::aptos_account::transfer',
+        functionArguments: [sender, 100],
       },
     });
     return txData;
+  };
+
+  const simulateTransaction = async () => {
+    const transactionData = await buildTransaction({ sender: account.address });
+    try {
+      const result = await aptos.transaction.simulate.simple({
+        signerPublicKey: account.publicKey,
+        transaction: transactionData,
+      });
+      console.log(result);
+    } catch (err) {
+      console.error('Simulate transaction error: ', err);
+    }
+  };
+
+  const simulateTransactionWithAccount = async () => {
+    const sender = Account.generate();
+    const receiver = Account.generate();
+
+    const config = new AptosConfig({ network: Network.TESTNET });
+    const aptosTestnet = new Aptos(config);
+
+    await aptosTestnet.fundAccount({
+      accountAddress: sender.accountAddress,
+      amount: 100_000_000,
+    });
+
+    // 1. Build the transaction to preview the impact of it
+    const transaction = await aptosTestnet.transaction.build.simple({
+      sender: sender.accountAddress,
+      data: {
+        // All transactions on Aptos are implemented via smart contracts.
+        function: '0x1::aptos_account::transfer',
+        functionArguments: [receiver.accountAddress, 100],
+      },
+    });
+
+    // 2. Simulate to see what would happen if we execute this transaction
+    const [userTransactionResponse] = await aptosTestnet.transaction.simulate.simple({
+      signerPublicKey: sender.publicKey,
+      transaction,
+    });
+    console.log('Simulate with Account.generate(): ', userTransactionResponse);
   };
 
   const handleSignTransaction = async () => {
@@ -81,14 +117,41 @@ function SignTransaction({ account, disabled }) {
   return (
     <Card title="Sign Transaction">
       <Space direction="vertical" style={{ width: '100%' }}>
-        <Button block onClick={handleSignTransaction} disabled={disabled} loading={signing}>
+        <Button
+          block
+          onClick={simulateTransactionWithAccount}
+          disabled={disabled}
+          loading={signing}
+        >
+          simulateTransactionWithAccount
+        </Button>
+
+        <Button
+          block
+          onClick={simulateTransaction}
+          disabled={disabled}
+          loading={signing}
+        >
+          simulateTransaction
+        </Button>
+
+        <Button
+          block
+          onClick={handleSignTransaction}
+          disabled={disabled}
+          loading={signing}
+        >
           signTransaction
         </Button>
 
-        <Button block onClick={handleSignAndSubmitTransaction} disabled={disabled} loading={signing}>
+        <Button
+          block
+          onClick={handleSignAndSubmitTransaction}
+          disabled={disabled}
+          loading={signing}
+        >
           signAndSubmitTransaction
         </Button>
-
       </Space>
     </Card>
   );
