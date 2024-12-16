e82491d9edb51ad1ad959461b58dd793af568f37
quanquan.luo <quanquan.luo@okg.com>
2024-12-16 11:46:39 +0800

    feat: add verify sig logic for solana

--- src/chains/Solana/components/SignMessage/index.js
+++ src/chains/Solana/components/SignMessage/index.js
@@ -1,19 +1,48 @@
 import {
   Button, Card, Space,
 } from 'antd-mobile';
+import { useState } from 'react';
+import nacl from 'tweetnacl';
+import { decodeUTF8 } from 'tweetnacl-util';
 import { toastFail, toastSuccess } from '../../../../utils/toast';
 
+const msg = 'Hello Solana';
+const encodedMsg = decodeUTF8(msg);
+
 function SignMessage({ account }) {
+  const [loading, setLoading] = useState(false);
+  const [signEdRet, setSignEdRet] = useState(null);
+
   const handleSignMsg = async () => {
     try {
-      const msg = 'Hello Solana';
-      const encodedMsg = new TextEncoder().encode(msg);
+      setLoading(true);
       const ret = await solana.signMessage(encodedMsg, 'utf8');
       console.log(ret);
+      setSignEdRet(ret);
       toastSuccess();
     } catch (error) {
       console.log(error);
       toastFail();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleVerifySignature = async () => {
+    try {
+      setLoading(true);
+      const verified = nacl.sign.detached.verify(
+        encodedMsg,
+        signEdRet.signature,
+        solana.publicKey.toBuffer(),
+      );
+      console.log(verified);
+      toastSuccess();
+    } catch (error) {
+      console.log(error);
+      toastFail();
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -21,8 +50,9 @@ function SignMessage({ account }) {
     <Card title="signMessage">
       <Space direction="vertical" style={{ width: '100%' }}>
         <Card direction="vertical">
-          <Space direction="vertical" style={{ width: '100%' }}>
-            <Button disabled={!account} block onClick={handleSignMsg}>签名</Button>
+          <Space style={{ width: '100%' }}>
+            <Button loading={loading} disabled={!account && !signEdRet} onClick={handleSignMsg}>signMessage</Button>
+            <Button loading={loading} disabled={!account || !signEdRet} onClick={handleVerifySignature}>verifySignature</Button>
           </Space>
         </Card>
       </Space>
